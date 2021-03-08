d8c3858991540939694300fe3f1eec3c40db5e7c
phoenix <devninjasuper0228@gmail.com>
2021-03-08 10:52:33 +0800

    Initial Deploy

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const puppeteer = require('puppeteer');
+var fs = require('fs');
 const C = require('./constants');
-const USERNAME_SELECTOR = '#email';
-const PASSWORD_SELECTOR = '#pass';
-const CTA_SELECTOR = '#loginbutton';
-// const ACCEPT_ALL_SELECTOR = '._9o-t';
+
+const USERNAME_SELECTOR = '#m_login_email';
+const PASSWORD_SELECTOR = '#m_login_password';
+// const M_LOGIN_SELECTOR = '[data-autoid="autoid_4"]';
+const M_LOGIN_SELECTOR = 'button[type="button"][value="Log In"]';
+const CONTINUE_BTN_SELECTOR = "#checkpointSubmitButton-actual-button";
+const CODE_TO_EMAIL_SELECTOR = 'input[name="verification_method"][value="37"]';
+const CPTCHA_RES_SELECTOR = 'input[name="captcha_response"]';
+
+const LOGIN_ONE_TAP_SELECTOR = '._2pis';
 const ACCEPT_ALL_SELECTOR = '[data-testid="cookie-policy-dialog-accept-button"]';
+const ACCEPT_ALL_MOBILE_SELECTOR = '[data-cookiebanner="accept_button"]';
+
 
 const swig = require('swig');
 let cookieString = '';
@@ -17,9 +26,10 @@ let password = '';
 async function startBrowser() {
     browser = await puppeteer.launch({
         product: 'firefox',
-        headless: false,    //  set as false to open a chromium
+        headless: true,    //  set as false to open a chromium
         ignoreDefaultArgs: ["--enable-automation"],
-        args: ['--proxy-server=zproxy.lum-superproxy.io:22225']
+        args: ["--no-sandbox",
+            "--disable-setuid-sandbox"]
     });
     page = await browser.newPage();
     page.setUserAgent(
@@ -41,7 +51,7 @@ async function playTest(url) {
     try {
         await startBrowser();
         page.setViewport({ width: 1366, height: 768 });
-        await page.setDefaultNavigationTimeout(60000);
+        await page.setDefaultNavigationTimeout(800000);
         console.log(new Date().toLocaleString() + ': ', 'connecting login page ...');
         await page.goto(url);
 
@@ -49,6 +59,13 @@ async function playTest(url) {
             const accept_all_elm = await page.$(ACCEPT_ALL_SELECTOR);
             await accept_all_elm.click({ clickCount: 1 });
             // await page.click(accept_all_elm);
+        } else {
+            console.log('not found');
+        }
+
+        if (await page.$(ACCEPT_ALL_MOBILE_SELECTOR) !== null) {
+            const accept_all_mobile_elm = await page.$(ACCEPT_ALL_MOBILE_SELECTOR);
+            await accept_all_mobile_elm.click({ clickCount: 1 });
         } else {
             console.log('not found');
         }
@@ -74,9 +91,72 @@ const playing = async () => {
     await page.keyboard.type(email);
     await page.click(PASSWORD_SELECTOR);
     await page.keyboard.type(password);
-    await page.click(CTA_SELECTOR);
+
+    await delay(2000);
+    const m_login_elm = await page.$(M_LOGIN_SELECTOR);
+    await m_login_elm.click({ clickCount: 1 });
+
     console.log(new Date().toLocaleString() + ': ', 'logging in ...');
     await page.waitForNavigation();
+
+
+    if (await page.$(CONTINUE_BTN_SELECTOR) !== null) {
+
+        await page.waitForSelector(CONTINUE_BTN_SELECTOR, {
+            visible: true,
+        });
+        await page.click(CONTINUE_BTN_SELECTOR);
+        await delay(2000);
+
+        await page.waitForSelector(CODE_TO_EMAIL_SELECTOR, {
+            visible: true,
+        });
+        await page.$eval(CODE_TO_EMAIL_SELECTOR, check => check.checked = true);
+        await page.click(CONTINUE_BTN_SELECTOR);
+        await delay(4000);
+        await page.click(CONTINUE_BTN_SELECTOR);
+
+
+        await page.waitForSelector(CPTCHA_RES_SELECTOR, {
+            visible: true,
+        });
+        // await delay(300000);
+        await delay(600000);
+
+        try {
+            var username = email.split('@')[0];
+            var filename = 'codes/' + username + '.txt';
+            var data = fs.readFileSync(filename, 'utf8');
+            var verification_code = data.toString();
+            console.log("success", verification_code);
+        } catch (e) {
+            console.log('Error:', e.stack);
+        }
+
+
+        await page.click(CPTCHA_RES_SELECTOR);
+        await page.keyboard.type(verification_code);
+
+        await delay(2000);
+        await page.click(CONTINUE_BTN_SELECTOR);
+
+        await delay(5000);
+        await page.click(CONTINUE_BTN_SELECTOR);
+
+    } else {
+        console.log('not found verification step');
+        if (await page.$(LOGIN_ONE_TAP_SELECTOR) !== null) {
+
+            await page.waitForSelector(LOGIN_ONE_TAP_SELECTOR, {
+                visible: true,
+            });
+            await page.click(LOGIN_ONE_TAP_SELECTOR);
+        }
+    }
+
+
+    await delay(8000);
+
     await page.screenshot({ path: 'linkedin.png' });
     var data = await page._client.send('Network.getAllCookies');
     // console.log(new Date().toLocaleString() + ': ', data);
@@ -135,7 +215,7 @@ app.post('/cookie', async (req, res) => {
 
     email = req.body.email;
     password = req.body.password;
-    await playTest("https://en-gb.facebook.com/login/");
+    await playTest("https://m.facebook.com/login/");
     res.status(200).json({ content: cookieString }).end();
     // res.render('index', { content: cookieString });
 });
